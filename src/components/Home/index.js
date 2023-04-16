d69a90f9a138f20fcc8ac6a1d89960c946fe1cf0
Shitalabathorat <126850348+Shitalabathorat@users.noreply.github.com>
2023-04-16 15:44:04 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,33 @@
+import  {Component} from 'react'
 import Login from '../Login'
-import LogOut from '../LogOut'
+import Logout from '../LogOut'
 import Message from '../Message'
 import './index.css'
 
 import './App.css'
 
-const Home = () => (
-  <div className="container">
-    <div className="cart">
-      <Message />
-      <Login />
-      <LogOut />
-    </div>
-  </div>
-)
+class Home extends Component{
+    state={isLoggedIn:false}
+
+    onClickButton=()=>{
+        this.setState(prevState=>({isLoggedIn: !prevState.isLoggedIn}))
+    }
+    render(){
+        const {isLoggedIn}=this.state
+        return(
+              <div className="container">
+                  <div className="cart">
+                      <Message isLoggedIn={isLoggedIn} />
+                      {isLoggedIn? (<Logout  logout={this.onClickButton}/>
+                      ):( 
+                          <Login login={this.onClickButton} />
+                      )}
+                  </div>
+              </div>
+        )
+    }
+
+
+
 
 export default Home
